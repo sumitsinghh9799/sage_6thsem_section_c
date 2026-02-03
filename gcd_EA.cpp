8415c9e36467b5cdfca1d9fc0d242257b8fb6a03
sumitsinghh9799 <sumitsingh11731@gmail.com>
2026-02-03 22:21:18 +0530

    notes

--- gcd_EA.cpp
+++ gcd_EA.cpp
@@ -1,6 +1,24 @@
 #include<iostream>
 using namespace std;
+int gcd(int a ,int b){
+    while(b!=0){
+        int t=a%b;
+        a=b;
+        b=t;
+    }
+    return a;
+}
 int main()
+{
+    int a,b;
+    cin>>a>>b;
+    while(b!=0){
+        int t=a%b;
+        a=b;
+        b=t;
+
+    } cout <<a<<endl;
+    cout<<gcd(a,b);
 
 return 0;
 }
