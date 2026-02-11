ee60472734f445dd6e5f4a260237e65543714b1f
sumitsinghh9799 <sumitsingh11731@gmail.com>
2026-02-11 14:59:41 +0530

    pattern

--- pattern/pattern6.cpp
+++ pattern/pattern6.cpp
@@ -4,14 +4,13 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-//reverse phyramid with number
-    for(int i=n; i>=1 ; --i)
-    {
-    for(int j=1; j<=i; ++j){
-                        
-            cout<<j<<"";
-        }
-        cout<<"\n";
+for ( int i=n; i>=1; i--){
+    //or for ( int j=1; j<= n-(i-1);j++)
+    for ( int j=1; j<=i; j++){
+        cout<<j<<" ";
+    }
+       
+        cout<<endl;
    }
    return 0;
 }
